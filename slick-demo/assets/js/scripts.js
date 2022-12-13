5072a1d4fc7100574ef70ecb19fd91845e29c659
WeatherlySB <sbweatherly@ualr.edu>
2022-12-13 03:27:49 +0000

    final submission

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
     $('.fade').slick({
       dots: true,
       infinite: true,
-      speed: 600,
+      speed: 500,
       fade: false,
       slide: 'div',
       cssEase: 'linear',
       autoplay: true,
       autoplaySpeed: 2000
+      
     });
 
-
-});
+  }
